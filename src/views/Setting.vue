ab73f82aab198155c50283df36aabe947b05de95
niarila <arielania@student.uns.ac.id>
2024-03-08 23:17:48 +0700

    update setting

--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -26,13 +26,47 @@
         </div>
       </div>
     </div>
+    <hr class="my-0" />
+    <div class="card-body">
+      <form id="formAccountSettings" method="POST" onsubmit="return false">
+        <div class="row">
+          <div class="mb-3 col-md-6">
+            <label for="firstName" class="form-label">First Name</label>
+            <input class="form-control" type="text" id="firstName" name="firstName" value="John" autofocus />
+          </div>
+          <div class="mb-3 col-md-6">
+            <label for="lastName" class="form-label">Last Name</label>
+            <input class="form-control" type="text" name="lastName" id="lastName" value="Doe" />
+          </div>
+          <div class="mb-3 col-md-6">
+            <label for="email" class="form-label">E-mail</label>
+            <input
+              class="form-control"
+              type="text"
+              id="email"
+              name="email"
+              value="john.doe@example.com"
+              placeholder="john.doe@example.com"
+            />
+          </div>
+          <div class="mb-3 col-md-6">
+            <label for="organization" class="form-label">Organization</label>
+            <input type="text" class="form-control" id="organization" name="organization" value="ThemeSelection" />
+          </div>
+          <div class="mt-2">
+            <button type="submit" class="btn btn-primary me-2">Save changes</button>
+            <button type="reset" class="btn btn-outline-secondary">Cancel</button>
+          </div>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import '../assets/vendor/css/core.css';
 import '../assets/vendor/css/theme-default.css';
-import "../assets/js/pages-account-settings-account.js";
+import '../assets/js/pages-account-settings-account.js';
 import '../assets/css/demo.css';
 </script>
 
