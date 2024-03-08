59988c0985731938485b16b6194d2e7c9095a4e9
niarila <arielania@student.uns.ac.id>
2024-03-08 22:05:35 +0700

    setting

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Login from '../views/Login.vue'
+import Setting from '../views/Setting.vue'
 
 const routes = [
   { path: '/', component: Login },
+  { path: '/', component: Setting }
 ];
 
 const router = createRouter({
