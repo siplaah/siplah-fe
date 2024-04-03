79bf311848312e7fad7017ff09e4d7f5368ee397
niarila <arielania@student.uns.ac.id>
2024-04-03 12:00:30 +0700

    update

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Login from '../views/Login.vue'
-import Setting from '../views/Setting.vue'
-import Setting2 from '../views/Setting2.vue'
-import DataJabatan from '../views/DataJabatan.vue'
-
-const routes = [
-  { path: '/', component: Login },
-  { path: '/setting', component: Setting },
-  { path: '/setting2', component: Setting2 },
-  { path: '/datajabatan', component: DataJabatan }
-
-];
+import { setupLayouts } from 'virtual:generated-layouts';
+import { createRouter, createWebHistory } from 'vue-router'
+import generatedRoutes from '~pages';
 
 const routes = setupLayouts(generatedRoutes)
 const router = createRouter({
