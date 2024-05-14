2466fc49cd87e44eab23c0f6e64b8484b86a9a9a
Ajengwahyu <ajengwahyu@student.uns.ac.id>
2024-05-14 18:29:47 +0700

    update component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="content-footer footer bg-footer-theme">
     <div class="container-xxl d-flex flex-wrap justify-content-between py-2 flex-md-row flex-column">
-      ASIH
+      © 2024 , made with ❤️ by dinosaurus
     </div>
   </footer>
 </template>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import Navbar from '../components/Navbar.vue';
+import Sidebar from '../components/Sidebar.vue';
+import Footer from '../components/Footer.vue';
 </script>
 
 <template>
-  <RouterView />
+  <div class="layout-wrapper layout-content-navbar">
+    <div class="layout-container">
+      <Sidebar></Sidebar>
+      <div class="layout-page">
+        <Navbar></Navbar>
+        <div class="content-wrapper">
+          <RouterView />
+          <Footer></Footer>
+          <div class="content-backdrop fade"></div>
+        </div>
+      </div>
+    </div>
+    <div class="layout-overlay layout-menu-toggle"></div>
+  </div>
 </template>
 
 
