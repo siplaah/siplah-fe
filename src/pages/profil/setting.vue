b5f90261ed12e2c812af857af21082071e65f8a5
Ajengwahyu <ajengwahyu@student.uns.ac.id>
2024-05-19 23:58:26 +0700

    Update setting.vue

--- src/pages/profil/setting.vue
+++ src/pages/profil/setting.vue
@@ -9,14 +9,14 @@
           <!-- Account -->
           <div class="card-body">
             <div class="d-flex align-items-start align-items-sm-center gap-4">
-              <img
+              <!-- <img
                 src="../../assets/img/avatars/1.png"
                 alt="user-avatar"
                 class="d-block rounded"
                 height="100"
                 width="100"
                 id="uploadedAvatar"
-              />
+              /> -->
               <div class="button-wrapper">
                 <label for="upload" class="btn btn-primary me-2 mb-4" tabindex="0">
                   <span class="d-none d-sm-block">Upload new photo</span>
